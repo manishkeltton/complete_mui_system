527110e6c86c0d95419f30fffcc4b1612140c1db
Manish Kumar <manish.kumar3@kelltontech.com>
2021-02-18 17:00:23 +0530

    SideBar,Header,Custmization Componenet

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,38 @@
 import './App.css';
 import SideMenu from '../components/SideMenu';
-import { CssBaseline, makeStyles } from '@material-ui/core';
+import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
+import { createMuiTheme, CssBaseline, makeStyles, ThemeProvider} from '@material-ui/core';
 import Header from '../components/Header';
+import PageHeader from '../components/PageHeader';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: "#3c44b126"
+    },
+    secondary: {
+      main: "#f83245",
+      light: "#f8324526"
+    },
+    background: {
+      default: "#f4f5fd"
+    }
+  },
+  overrides:{
+    MuiAppBar:{
+      root:{
+        transform:'translateZ(0)'
+      }
+    }
+  },
+  props:{
+    MuiIconButton:{
+      disableRipple:true
+    }
+  }
+})
 
 const useStyles = makeStyles({
   appMain:{
@@ -15,14 +46,19 @@ function App() {
   const classes = useStyles();
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <SideMenu />
       <div className={classes.appMain}>
           <Header />
+          <PageHeader 
+          title="Page Header"
+          subTitle="Page description"
+          icon = {<PeopleOutlineTwoToneIcon fontSize='large'/>}
+          />
       </div>
       <CssBaseline />
       
-    </>
+    </ThemeProvider>
   );
 }
 
