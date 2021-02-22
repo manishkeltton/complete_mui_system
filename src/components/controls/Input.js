077c3041e47894dfccc6edfb88714d3cfd49c5c8
Manish Kumar <manish.kumar3@kelltontech.com>
2021-02-22 15:24:27 +0530

    Use Table,Pagination, Sorting

--- src/components/controls/Input.js
+++ src/components/controls/Input.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Input(props) {
  
-    const { name, label, value, error=null, onChange } = props;
+    const { name, label, value, error=null, onChange, ...other } = props;
 
     return(
         <TextField 
@@ -12,6 +12,7 @@ export default function Input(props) {
             name={name}
             value={value}
             onChange = {onChange}
+            {...other}
             {...(error && {error:true, helperText:error})}
         />
     )
--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -2,14 +2,19 @@ import React, { useState } from 'react'
 import EmployeesForm from './EmployeesForm';
 import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
 import PageHeader from '../../components/PageHeader';
-import { makeStyles, Paper, TableBody, TableCell, TableRow } from '@material-ui/core';
+import { InputAdornment, makeStyles, Paper, TableBody, TableCell, TableRow, Toolbar } from '@material-ui/core';
 import useTable from '../../components/controls/useTable';
 import * as employeeService from "../../services/employeeService";
+import Controls from '../../components/controls/Controls';
+import { Search } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme =>({
     pageContent:{
         margin:theme.spacing(5),
         padding:theme.spacing(3)
+    },
+    searchInput:{
+        width:'75%'
     }
 }))
 
@@ -17,18 +22,33 @@ const headCells = [
     {id:'fullName',label:'Employee Name'},
     {id:'email',label:'Email Address (personal)'},
     {id:'mobile',label:'Mobile Number'},
-    {id:'deparment',label:'Department '}
+    {id:'deparment',label:'Department',disableSorting:true}
 ]
 
 export default function Employees(){
 
     const classes = useStyles();
     const [records,setRecords] = useState(employeeService.getAllEmployees());
+    const [filterFn, setfilterFn] = useState({ fn: items => { return items; } })
 
     const {
         TblContainer,
-        TblHead
-    } = useTable(records,headCells);
+        TblHead,
+        TblPagination,
+        recordsAfterPagingAndSorting
+    } = useTable(records,headCells,filterFn);
+
+    const handleSearch = e =>  {
+        let target = e.target;
+        setfilterFn({
+            fn: items => {
+                if(target.value == "")
+                    return items;
+                else 
+                    return items.filter(x => x.fullName.toLowerCase().includes(target.value))
+            }
+        })
+    }
 
     return (
     <>
@@ -39,11 +59,23 @@ export default function Employees(){
         />
         <Paper className={classes.pageContent}>
         {/* <EmployeesForm /> */}
+        <Toolbar>
+            <Controls.Input
+                label="Search Employees"
+                className = {classes.searchInput} 
+                InputProps={{
+                    startAdorment: (<InputAdornment position="start">
+                        <Search />
+                    </InputAdornment>)
+               }}
+               onChange={ handleSearch }
+            />
+        </Toolbar>
         <TblContainer>
             <TblHead />
             <TableBody>
                 {
-                    records.map(item =>
+                    recordsAfterPagingAndSorting().map(item =>
                         (<TableRow key={item.id}>
                             <TableCell>{item.fullName}</TableCell>
                             <TableCell>{item.email}</TableCell>
@@ -54,6 +86,7 @@ export default function Employees(){
                 }
             </TableBody>
         </TblContainer>
+        <TblPagination />
         </Paper>
     </>
     )
