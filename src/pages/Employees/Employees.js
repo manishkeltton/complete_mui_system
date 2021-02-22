c4380e49ea0055fd8db4365460a4711d00c76111
Manish Kumar <manish.kumar3@kelltontech.com>
2021-02-22 09:01:09 +0530

    use Table

--- src/pages/Employees/Employees.js
+++ src/pages/Employees/Employees.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EmployeesForm from './EmployeesForm';
 import PeopleOutlineTwoToneIcon from '@material-ui/icons/PeopleOutlineTwoTone';
 import PageHeader from '../../components/PageHeader';
-import { makeStyles, Paper } from '@material-ui/core';
+import { makeStyles, Paper, TableBody, TableCell, TableRow } from '@material-ui/core';
+import useTable from '../../components/controls/useTable';
+import * as employeeService from "../../services/employeeService";
 
 const useStyles = makeStyles(theme =>({
     pageContent:{
@@ -11,9 +13,22 @@ const useStyles = makeStyles(theme =>({
     }
 }))
 
+const headCells = [
+    {id:'fullName',label:'Employee Name'},
+    {id:'email',label:'Email Address (personal)'},
+    {id:'mobile',label:'Mobile Number'},
+    {id:'deparment',label:'Department '}
+]
+
 export default function Employees(){
 
     const classes = useStyles();
+    const [records,setRecords] = useState(employeeService.getAllEmployees());
+
+    const {
+        TblContainer,
+        TblHead
+    } = useTable(records,headCells);
 
     return (
     <>
@@ -23,7 +38,22 @@ export default function Employees(){
         icon = {<PeopleOutlineTwoToneIcon fontSize='large'/>}
         />
         <Paper className={classes.pageContent}>
-        <EmployeesForm />
+        {/* <EmployeesForm /> */}
+        <TblContainer>
+            <TblHead />
+            <TableBody>
+                {
+                    records.map(item =>
+                        (<TableRow key={item.id}>
+                            <TableCell>{item.fullName}</TableCell>
+                            <TableCell>{item.email}</TableCell>
+                            <TableCell>{item.mobile}</TableCell>
+                            <TableCell>{item.department}</TableCell>
+                        </TableRow>)
+                        )
+                }
+            </TableBody>
+        </TblContainer>
         </Paper>
     </>
     )
